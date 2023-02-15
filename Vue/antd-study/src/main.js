89e0eb03644406fa679341bdec3ace389285ff60
LireEruel <jsh001505@gmail.com>
2023-02-15 18:00:26 +0900

    typescript로 프로젝트 재생성

--- Vue/antd-study/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import Antd from "ant-design-vue";
-import App from "./App";
-import "ant-design-vue/dist/antd.css";
-import "./assets/global.css";
-
-const app = createApp(App);
-
-app.use(Antd).mount("#app");
