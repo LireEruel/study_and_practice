862865e6147afb7a623e237da23bc81142fdb3f1
LireEruel <jsh001505@gmail.com>
2023-03-06 14:29:59 +0900

    appendBox 기능 추가

--- HTML+CSS+JS/css-example/index.js
+++ HTML+CSS+JS/css-example/index.js
@@ -1,5 +1,5 @@
 import Header from "./components/header/Header.js";
-//import Main from "../components/main/Main.js";
+import Main from "./components/main/Main.js";
 
 new Header(document.querySelector("body"));
-//const main = new Main(document.querySelector("body"));
+new Main(document.querySelector("body"));
