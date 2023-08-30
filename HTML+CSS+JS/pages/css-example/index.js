02e50d4e7f075b04bc3f9c2d5f2c6c289bf86d54
LireEruel <jsh001505@gmail.com>
2023-08-30 20:24:37 +0900

    필요없는 파일 삭제 및 업로드

--- HTML+CSS+JS/pages/css-example/index.js
+++
@@ -1,5 +0,0 @@
-import Header from "./components/header/Header.js";
-import Main from "./components/main/Main.js";
-
-new Header(document.querySelector("body"));
-new Main(document.querySelector("body"));
